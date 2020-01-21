1abd8e6c2aca47bbe0a9184b4df9f48791579844
jhuang429 <jhuang9240@gmail.com>
2020-01-21 15:27:28 -0500

    update test.rb

--- test.rb
+++ test.rb
@@ -18,14 +18,28 @@ prompt = TTY::Prompt.new
 # end
 
 def search(string)
+    prompt = TTY::Prompt.new
     result = RestClient.get("https://www.episodate.com/api/search?q=#{string}&page=1")
     result = JSON.parse(result)
+    arr = []
     result["tv_shows"].each_with_index do |show,index|
-        #puts "#{index+1}. #{show["name"]}"
-        pp result
+        arr << "#{index+1}. #{show["name"]}"
     end
+    arr << "Exit"
+    choice = prompt.select("action?", arr)
 end
 
-search("rick")
+def picked_show_prompt(show)
+    system("clear")
+    prompt = TTY::Prompt.new
+    options = ["Favorite show", "Find Out More Info", "Exit"]
+    prompt.select(show, options)
+end
+
+
+user_input = search("rick")
+
+picked_show_prompt(user_input)
+
 
 
