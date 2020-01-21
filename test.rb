83f77588e669fdf3fec3525685478e6a5bded2c7
jhuang429 <jhuang9240@gmail.com>
2020-01-21 15:58:28 -0500

    new changes

--- test.rb
+++ test.rb
@@ -4,18 +4,18 @@ require 'JSON'
 
 prompt = TTY::Prompt.new
 
-# system("clear")
-# puts 'WELCOME TO TV CHUG'
-# puts 
-# prompt.select("Chose an option",  %w(Sign_In New_Account View_as_Guest))
+system("clear")
+puts 'WELCOME TO TV CHUG'
+puts 
+prompt.select("Chose an option",  %w(Sign_In New_Account View_as_Guest))
 
-# popular_shows = RestClient.get("https://www.episodate.com/api/most-popular?page=1")
+popular_shows = RestClient.get("https://www.episodate.com/api/most-popular?page=1")
 
-# popular_shows = JSON.parse(popular_shows)
+popular_shows = JSON.parse(popular_shows)
 
-# popular_shows["tv_shows"].each_with_index do |show,index|
-#     puts "#{index+1}. #{show["name"]}"
-# end
+popular_shows["tv_shows"].each_with_index do |show,index|
+    puts "#{index+1}. #{show["name"]}"
+end
 
 def search(string)
     prompt = TTY::Prompt.new
