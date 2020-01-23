72ab94e53d730faf545f8f6adee14c6bb63a1888
Nathan Brickett <natey37@gmail.com>
2020-01-23 15:47:19 -0500

    dad2

---
+++ lib/pages/shoho_menu.rb
@@ -0,0 +1,35 @@
+def sho_ho
+    random_number = rand(68366)
+    result = RestClient.get("https://www.episodate.com/api/show-details?q=#{random_number}")
+    result = JSON.parse(result)
+    puts "Name: #{result["tvShow"]["name"]}"
+    puts "Description: #{result["tvShow"]["description"]}"
+    puts "Start Date: #{result["tvShow"]["start_date"]}"
+    puts "End Date: #{result["tvShow"]["end_date"]}"
+    puts "Network: #{result["tvShow"]["network"]}"
+    puts "Status: #{result["tvShow"]["status"]}"
+    puts "Trailer: https://www.youtube.com/watch?v=#{result["tvShow"]["youtube_link"]}"
+    puts "Genres: #{result["tvShow"]["genres"]}"
+    puts "# of Episodes: #{result["tvShow"]["episodes"].count}"
+
+    $current_show_hash = {
+        name: "#{result["tvShow"]["name"]}", 
+        start_date: "#{result["tvShow"]["start_date"]}",
+        end_date: "#{result["tvShow"]["end_date"]}",
+        network: "#{result["tvShow"]["network"]}",
+        status: "#{result["tvShow"]["status"]}",
+        permalink: "https://www.youtube.com/watch?v=#{result["tvShow"]["youtube_link"]}",
+        episodate_id: "#{random_number}"
+    }
+
+    if $current_user == "Guest"
+        action = ttyprompt("Options",["Favorite This Show", "See Top Shows", "Search Shows", "Exit"])
+        user_action(action)  
+    elsif $current_user.has_show_episodate_id_in_fav($current_show_id)
+        action = ttyprompt("Options",["Delete Favorite", "Sho'ho", "Search Shows", "Exit"])
+        user_action(action)
+    else
+        action = ttyprompt("Options",["Favorite This Show", "Sho'ho", "Search Shows", "Exit"])
+        user_action(action)
+    end
+end 
